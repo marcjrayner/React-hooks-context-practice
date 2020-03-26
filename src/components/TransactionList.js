cafe01c8bbbc9e9d93c6e1f3fcb4691d70ec8e3a
marcjrayner <26255479+marcjrayner@users.noreply.github.com>
2020-03-26 10:20:37 +0000

    adds context to transaction list

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext)
+  
   return (
     <>
     <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>=$400</span><button className="delete-btn">X</button>
+        {transactions.map(transaction => (
+        <li className="minus" key={transaction.id}>
+          {transaction.text} <span>${transaction.amount}</span><button className="delete-btn">X</button>
         </li>
+          
+        ))}
       </ul>
     </>
   )
