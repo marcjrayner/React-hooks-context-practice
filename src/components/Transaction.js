245b533cc1b96e711ff3a9c22666a5cd0d727226
marcjrayner <26255479+marcjrayner@users.noreply.github.com>
2020-03-26 11:41:35 +0000

    adds deleteTransaction to globalcontext

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+'
+
+  const { deleteTransaction } = useContext(GlobalContext)
+
   return (
     <div>
       <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
         {transaction.text} 
         <span>{sign}${Math.abs(transaction.amount)}</span>
-        <button className="delete-btn">X</button>
+        <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>X</button>
       </li>
     </div>
   )
